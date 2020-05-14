d50e2b85d39986fde61b20cd26984e5b84c08c2c
humberto <humbertorivero9@gmail.com>
2020-05-14 00:04:48 -0300

    pagina de noticias

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log ("Hola Mundo")
+document.querySelector('.menu-btn').addEventListener('click', () =>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+})
+
+// scroll reveal IMPORTANTE
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards' , {delay: 500});
+ScrollReveal().reveal('.cards-banner-one', {delay: 500});
+ScrollReveal().reveal('..cards-banner-two', {delay: 500});
